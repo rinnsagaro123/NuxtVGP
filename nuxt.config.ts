b47f4695b6ab6f28a9a3ecfa35d352e8ac6d44f6
rinnsagaro123 <angelo.lerumg240403@gmail.com>
2025-01-12 22:46:03 +0800

    chore(setup): initialize project setup with Nuxt configuration
    This commit includes the initial setup for the Nuxt project, including the package.json for dependencies, nuxt.config.ts for configuration, and package-lock.json to lock dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,41 @@
 import vuetify from 'vite-plugin-vuetify'
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import type { NuxtPage } from 'nuxt/schema'
 
 export default defineNuxtConfig({
-	future: { compatibilityVersion: 4 },
-	build: { transpile: ['vuetify'] },
-	imports: { dirs: ['./stores'] },
+  
+  future: { compatibilityVersion: 4 },
+  build: { transpile: ['vuetify'] },
+  imports: { dirs: ['./stores'] },
+  router: {
+    middleware: ['redirectToLaunch'] 
+  },
+  
 
-	apollo: {
-		autoImports: true,
-		proxyCookies: true,
-		clients: {
-			default: { httpEndpoint: 'https://spacex-production.up.railway.app/' },
-		},
-	},
+  apollo: {
+    autoImports: true,
+    proxyCookies: true,
+    clients: {
+      default: { httpEndpoint: 'https://spacex-production.up.railway.app/' },
+    },
+  },
 
-	vite: {
-		optimizeDeps: {
-			include: ['graphql-tag'],
-		},
-		plugins: [vuetify()],
-	},
+  vite: {
+    optimizeDeps: {
+      include: ['graphql-tag'],
+    },
+    plugins: [vuetify()],
+  },
 
-	modules: ['@nuxtjs/apollo', ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }]],
-	compatibilityDate: '2024-11-11',
-})
+  modules: [
+    '@nuxtjs/apollo',
+    ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }],
+    '@nuxtjs/color-mode',
+  ],
+  
+  compatibilityDate: '2024-11-11',
+
+  typescript: {
+    shim: false, 
+  },
+  
+});
